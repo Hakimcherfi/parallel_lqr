c72e4f066b28e689bf1ffa8bdd733d4040aa97a0
Hakimcherfi <hakimcherfi31@gmail.com>
2021-06-11 16:34:14 +0200

    Bugs corrigés, affichage, attention instabilité

--- parallel_lqr.py
+++ parallel_lqr.py
@@ -7,24 +7,27 @@ from scipy.linalg import null_space,orth
 
 T = 100 #T+1 points
 dt = 0.01
+
 x0 = np.array([[0.],[0.],[0.],[0.]]) #contrainte
-xtarg = np.array([[1.],[0.],[0.],[0.]])
-xweight = 0.1
+xtarg = np.array([[2.],[1.],[0.],[0.]]) #fonction cout
+xterm = np.array([[2.],[1.],[0.],[0.]]) #supposition
+
+xweight = 1.
 uweight = 1.
-xweightT = 1000.
-J = 2
-tau = int(T/2)
 
 nq = 2
 nv = 2
 n = nq+nv
 m = 2
+dimspace = 2 #pour affichage
+
+#dynamique
 Fx = np.eye(n)
 Fx[:nv,nv:]=dt*np.eye(nv)
-Fu = np.concatenate([0.5*dt**2*np.eye(nv),dt*np.eye(nv)])
 
-xterm = np.array([[0.5],[0.],[1.],[0.]]) #supposition
-                
+#Fu = np.concatenate([0.5*dt**2*np.eye(nv),dt*np.eye(nv)]) #rend instable
+Fu = np.concatenate([np.zeros((nq,nq)),dt*np.eye(nv)])
+
 def costx(x):
     Cx = xweight*np.eye(n)
     return 0.5*(x-xtarg).T@Cx@(x-xtarg)
@@ -36,17 +39,13 @@ def costu(u):
 def cost(x,u):
     return costx(x)+costu(u)
 
-def finalcost(x):
-    CxT = xweightT*np.eye(n)
-    return 0.5*(x-xtarg).T@CxT@(x-xtarg)
-
 def next_state(x,u):
     return Fx@x + Fu@u
-    
+
 def Qxx(x=np.zeros((n,1))):
     return hessien(costx)(x)
 
-def Quu(u):
+def Quu(u=np.zeros((m,1))):
     return hessien(costu)(u)
 
 def gradient(f):
@@ -79,110 +78,125 @@ def hessien(f):
     return fbis
 
 
-def subproblem(xinit,xterm,T):
-    
-    x=np.tile(xinit,(1,T+1))
-    x[:,T:T+1] = xterm
-    u=np.zeros((m,T))
-    
+def subgains(xterm,T):
     # a t = T (T du sous probleme)
-    Hx = [np.eye(n)]
-    h1 = [np.zeros((n,1))]
-    vx1 = [np.zeros((n,1))]
-    Vzx = [np.zeros((n,n))]
-    Hz = [-1.*np.eye((n))]
-    #Vxx = [hessien(finalcost)(xterm)]
-    Vxx = [hessien(costx)(xterm)]
-    vz1 = [np.zeros((n,1))]
-    Vzz = [np.zeros((n,n))]
+    Vxx = Qxx()
+    Vzx = np.zeros((n,n))
+    Vzz = np.zeros((n,n))
+    vx1 = np.zeros((n,1)) #+qx1T
+    vz1 = np.zeros((n,1))
     
-    mx1 = []
-    mz1 = []
-    Mzu = []
-    Mxx = []
-    Mux = []
-    Nu = []
-    Nz = []
-    mu1 = []
-    Mzx = []
-    Mzz = []
-    Muu = []
-    Nx = []
-    n1 = []
-    Zw = []
-    Py = []
-    #y = []
-    #w = []
-    Kx = []
-    Kz = []
-    k1 = []
+    Hx = np.eye(n)
+    Hz = -1.*np.eye((n))
+    h1 = np.zeros((n,1))
+
+    Kxl = []
+    Kzl= []
+    k1l = [] 
     
     for t in range(T-1,-1,-1):
         # a t < T
-        print(t)
-        mx1.append(Fx.T@vx1[-1]) # + qx1T
-        assert mx1[-1].shape == ((n,1))
+        Mxx = Qxx()+Fx.T@Vxx@Fx
+        Mux = Fu.T@Vxx@Fx # + Qux
+        Mzu = Vzx@Fu
+        Mzx = Vzx@Fx
+        Mzz = Vzz
+        Muu = Quu() + Fu.T@Vxx@Fu
+        mx1 = Fx.T@vx1 # + qx1
+        mu1 = Fu.T@vx1 #+qu1
+        mz1 = vz1
+        Nx = Hx@Fx
+        Nu = Hx@Fu
+        Nz = Hz
+        n1 = h1 #+Hx@np.zeros((n,1))
+        Zw = null_space(Nu)
+        Py = orth(Nu.T) #regarder numpy qr...
+        A = Py@linalg.pinv(Nu@Py)
+        B = Zw@linalg.inv(Zw.T@Muu@Zw)@(Zw.T)
         
-        mz1.append(vz1[-1])
-        assert mz1[-1].shape == ((n,1))
-        
-        Mzu.append(Vzx[-1]@Fu)
-        assert Mzu[-1].shape == ((n,m))
-        
-        Mxx.append(Qxx(x[:,t:t+1]) + Fx.T@Vxx[-1]@Fx)
-        assert Mxx[-1].shape == ((n,n))
-        
-        Mux.append(Fu.T@Vxx[-1]@Fx) # + Quxt
-        assert Mux[-1].shape == ((m,n))
-        
-        Nu.append(Hx[-1]@Fu)
-        assert Nu[-1].shape == ((n,m))
-        
-        Nz.append(Hz[-1])
-        assert Nz[-1].shape == ((n,n))
-        
-        mu1.append(Fu.T@vx1[-1]) #+qu1t
-        assert mu1[-1].shape == ((m,1))
-        
-        Mzx.append(Vzx[-1]@Fx)
-        assert Mzx[-1].shape == ((n,n))
-        
-        Mzz.append(Vzz[-1])
-        assert Vzz[-1].shape == ((n,n))
-        
-        Muu.append(Quu(u[:,t:t+1]) + Fu.T@Vxx[-1]@Fu)
-        assert Muu[-1].shape == ((m,m))
-        
-        Nx.append(Hx[-1]@Fx)
-        assert Nx[-1].shape == ((n,n))
-        
-        n1.append(h1[-1]) #+Hx[-1]@np.zeros((n,1))
-        assert n1[-1].shape == ((n,1))
-        
-        Zw.append(null_space(Nu[-1]))
-        Py.append(orth(Nu[-1].T)) #regarder numpy qr...
-        #if (Py[-1].size>0):
-        #    y.append(-linalg.pinv(Nu[-1]@Py[-1])@(Nx[-1]@x[:,t:t+1]+Nz[-1]@xterm+n1[-1]))
-        #else:
-        #    y.append([])
-        #if (Zw[-1].size>0):
-        #    w.append(-linalg.inv(Zw[-1].T@Muu[-1]@Zw[-1])@(Zw[-1].T)@(Mux[-1]@x[:,t:t+1]+Mzu[-1].T@xterm+mu1[-1]))
-        #else:
-        #    w.append([])
-        
-        Kx.append(-(Py[-1]@linalg.pinv(Nu[-1]@Py[-1])@Nx[-1]+Zw[-1]@linalg.inv(Zw[-1].T@Muu[-1]@Zw[-1])@(Zw[-1].T)@Mux[-1]))
-        Kz.append(-(Py[-1]@linalg.pinv(Nu[-1]@Py[-1])@Nz[-1]+Zw[-1]@linalg.inv(Zw[-1].T@Muu[-1]@Zw[-1])@(Zw[-1].T)@Mzu[-1].T))
-        k1.append(-(Py[-1]@linalg.pinv(Nu[-1]@Py[-1])@n1[-1]+Zw[-1]@linalg.inv(Zw[-1].T@Muu[-1]@Zw[-1])@(Zw[-1].T)@mu1[-1]))
+        #calcul gains
+        Kx = -A@Nx -B@Mux
+        Kz = -A@Nz -B@(Mzu.T)
+        k1 = -A@n1 -B@mu1
+        Kxl.append(Kx)
+        Kzl.append(Kz)
+        k1l.append(k1)
+        assert Py.shape!=((m,1))
+        if (True):
+            print("t={}".format(t))
+            print("Nu={}".format(Nu))
+            print("Py={}".format(Py))
+            print("Zw={}".format(Zw))
+            print("A={}".format(A))
+            print("B={}".format(B))
+            if (Py.shape==((m,m))):
+                print("Py==Im ? : {}".format((Py==np.eye(m)).all()))
+            print("A*Nu == Im ? : {}".format((A@Nu==np.eye(m)).all()))
+            print("A*Nu is close to Im ? : {}".format(np.allclose(A@Nu,np.eye(m))))
+            
+        #maj cout
+        Vxx = Mxx+Mux.T@Kx+Kx.T@Mux+Kx.T@Muu@Kx
+        Vzx = Mzx+Mzu@Kx+Kz.T@Mux+Kz.T@Muu@Kx
+        Vzz = Mzz+Kz.T@Mzu.T+Mzu@Kz+Kz.T@Muu@Kz
+        vx1 = mx1+Kx.T@mu1+(Mux.T+Kx.T@Muu)@k1
+        vz1 = mz1+Mzu@k1+Kz.T@mu1+Kz.T@Muu@k1
+        Hx = (np.eye(n)-Nu@A)@Nx
+        Hz = (np.eye(n)-Nu@A)@Nz
+        h1 = (np.eye(n)-Nu@A)@n1
+    return Kxl,Kzl,k1l
+
+def subroll(x0,xterm,Kx,Kz,k1):
+    T = len(Kx)
+    x = np.zeros((n,T+1))
+    x[:,0:1] = x0
+    x[:,-2:-1] = xterm
+    u = np.zeros((m,T))
+    for t in range(T):
         u[:,t:t+1] = Kx[-1]@x[:,t:t+1]+Kz[-1]@xterm+k1[-1]
-        Hx.append((np.eye(n)-Nu[-1]@Py[-1]@linalg.pinv(Nu[-1]@Py[-1]))@Nx[-1])
-        Hz.append((np.eye(n)-Nu[-1]@Py[-1]@linalg.pinv(Nu[-1]@Py[-1]))@Nz[-1])
-        h1.append((np.eye(n)-Nu[-1]@Py[-1]@linalg.pinv(Nu[-1]@Py[-1]))@n1[-1])
-        Vxx.append(Mxx[-1]+2.*Mux[-1].T@Kx[-1]+Kx[-1].T@Muu[-1]@Kx[-1])
-        Vzz.append(Mzz[-1]+2.*Mzu[-1]@Kz[-1]+Kz[-1].T@Muu[-1]@Kz[-1])
-        Vzx.append(Mzx[-1]+Mzu[-1]@Kx[-1]+Kz[-1].T@Mux[-1] +Kz[-1].T@Muu[-1]@Kx[-1])
-        vx1.append(mx1[-1]+Kx[-1].T@mu1[-1]+(Mux[-1].T+Kx[-1].T@Muu[-1])@k1[-1])
-        vz1.append(mz1[-1]+Mzu[-1]@k1[-1]+Kz[-1].T@mu1[-1]+Kz[-1].T@Muu[-1]@k1[-1])
-        #pdb.set_trace()
-    print(u)
+        x[:,t+1:t+2] = next_state(x[:,t:t+1],u[:,t:t+1])
+        Kx.pop()
+        Kz.pop()
+        k1.pop()
+    return x,u
+
+def constrainedLQR(x0,xterm,T):
+    Kxl,Kzl,k1l = subgains(xterm,T)
+    return subroll(x0,xterm,Kxl,Kzl,k1l)
+
+def scatter_x(x,cercle=False):
+    plt.figure()
+    figure, axes = plt.subplots()
+    if (dimspace==2):
+        if(cercle):
+            draw_circle = plt.Circle((xsphere[0:1,:], xsphere[1:2,:]), Rsphere,fill=False)
+            draw_circle2 = plt.Circle((xsphere[0:1,:], xsphere[1:2,:]), Rsphere+distsecu,fill=False)
+            axes.add_artist(draw_circle)
+            axes.add_artist(draw_circle2)
+        plt.xlabel("x1")
+        plt.ylabel("x2")
+        plt.scatter(x[0:1,:],x[1:2,:],s=3)
+        plt.title("Trajectoire")
+        plt.savefig("trajectoire.png")
+    if(dimspace==1):
+        plt.scatter(x[0:1,:],np.zeros((1,x[0:1,:].shape[1])))
+        plt.title("Trajectoire")
     
-subproblem(x0,xtarg,T)
+def lines(x,u):
+    t = np.linspace(0,T*dt,T+1)
+    fig,(ax1,ax2,ax3)=plt.subplots(3,1,sharex=True)
+    for k in range(dimspace):
+        ax1.plot(t,x[k,:],label="x"+str(k+1))
+        ax2.plot(t,x[dimspace+k,:],label="v"+str(k+1))
+        ax3.plot(t[:-1],u[k,:],label="u"+str(k+1))
+    ax1.legend()
+    ax2.legend()
+    ax3.legend()
+    fig.suptitle("Courbes")
+    plt.savefig("courbes.png",dpi=1000)
+
+def calcul_cout_et_affichage(x,u):
+    scatter_x(x,False)
+    lines(x,u)
+
+x,u = constrainedLQR(x0,xterm,T)
+calcul_cout_et_affichage(x,u)
